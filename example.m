4e90c7c327f992cd459f352638220e76cc1284bc
YonatanRosen <roseny77@yahoo.com>
2015-03-19 18:24:57 +0200

    Update example.m

--- example.m
+++ example.m
@@ -1,6 +1,5 @@
 load c_elegans;
 addpath(genpath(pwd));
-Graph = ObjectCreateGraph(g);
 [total_motifs3, single_vertex_motifs3] = count3motifs(g);
 [total_motifs4, single_vertex_motifs4] = count4motifs(g);
 
